1b30d93645112918dc9e7151647e018e7b5fae73
Mihir Prakash <mprakash@altimetrik.com>
2020-07-30 18:20:14 +0530

    Updates

---
+++ precache-manifest.72eb877546ee1f4bbeeaa18c3b8ddac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c12cc3c6a110e00ff209b9ddaf080a",
+    "url": "/SampleDemo/index.html"
+  },
+  {
+    "revision": "21aa11e409504271942e",
+    "url": "/SampleDemo/static/css/main.677e454a.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/SampleDemo/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SampleDemo/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21aa11e409504271942e",
+    "url": "/SampleDemo/static/js/main.2e6f4349.chunk.js"
+  },
+  {
+    "revision": "aa25c1b27a2c99902c46",
+    "url": "/SampleDemo/static/js/runtime-main.43b9bd21.js"
+  }
+]);
