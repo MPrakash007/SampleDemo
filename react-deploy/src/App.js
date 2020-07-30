0ed8de0e7681281b7344c88ba5edb7ec6a34d2e6
Mihir Prakash <mprakash@altimetrik.com>
2020-07-30 18:15:26 +0530

    first push

---
+++ react-deploy/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>My First React app</h1>
+     <br></br>
+     <h2>My Name is Mihir</h2>
+     <h3>HYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY</h3>
+
+    </div>
+  );
+}
+
+export default App;
